dcfcd213b15de2b48e0595cae9b024e249dc0fe6
Matav <verner.matej@gmail.com>
2023-03-11 23:59:23 +0100

    Map Prefab, SceneLoader

--- Assets/Scripts/Misc/SceneLoader.cs
+++ Assets/Scripts/Misc/SceneLoader.cs
@@ -14,10 +14,27 @@ public class SceneLoader : SceneSingleton<SceneLoader>
     {
         StartCoroutine(RestartCoroutine());
     }
+    public void LoadLevel(int level, int part)
+    {
+        StartCoroutine(LoadLevelCoroutine(level,part));
+
+    }
+    private IEnumerator LoadLevelCoroutine(int level, int part)
+    {
+        if (UIManager.HasInstance() && UIManager.Instance.FadeAnimation != null)
+        {
+            UIManager.Instance.FadeAnimation.FadeOut();
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene($"Level-{level}-{part}");
+        }
+    }
     private IEnumerator RestartCoroutine()
     {
-        UIManager.Instance.FadeAnimation.FadeOut();
-        yield return new WaitForSeconds(1);
+        if (UIManager.HasInstance() && UIManager.Instance.FadeAnimation != null)
+        {
+            UIManager.Instance.FadeAnimation.FadeOut();
+            yield return new WaitForSeconds(1);
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     }
