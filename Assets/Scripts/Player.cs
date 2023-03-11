3ffdba7a90c6f787fc05234b53e066b15d528c4d
vratislavino <vratislavino@gmail.com>
2023-03-11 23:39:32 +0100

    Player fcked merge fixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,6 +9,8 @@ public class Player : SceneSingleton<Player>
 {
     private Rigidbody2D body;
     public Rigidbody2D Body => body;
+    public CinemachineVirtualCamera PlayerCamera;
+    public Volcano Volcano;
 
     protected override void Awake()
     {
@@ -16,27 +18,31 @@ public class Player : SceneSingleton<Player>
     }
     private void Start()
     {
-        
+        Vector2 dir = Map.Instance.StarterBoost.directionPoint.position - transform.position;
+        Body.AddForce(dir.normalized * 100);
     }
     private void FixedUpdate()
     {
 
     }
-    public void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
+    public void Update() {
+        if (Input.GetMouseButtonDown(0)) {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit)
-            {
+            if (hit) {
                 IInteractable interactable = hit.rigidbody.GetComponent<IInteractable>();
-                if (interactable != null)
-                {
+                if (interactable != null) {
                     interactable.OnButtonDown();
                     StartCoroutine(Wait(interactable));
+                } else {
+                    if (Volcano != null)
+                        Volcano.StartFiring();
                 }
+            } else {
+                if (Volcano != null)
+                    Volcano.StartFiring();
             }
         }
+
     }
     public IEnumerator Wait(IInteractable hit)
     {
@@ -51,7 +57,7 @@ public class Player : SceneSingleton<Player>
     }
 
     internal void GameOver(CauseOfDeath causeOfDeath = CauseOfDeath.NotSpecified) {
-
+        SceneLoader.Instance.Restart();
     }
 }
 
